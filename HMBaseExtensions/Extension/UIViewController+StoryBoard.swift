da86209f2199be817795b1fae5ebf1621fea930e
Hovhannes Stepanyan <hovhannes@helix.am>
2019-09-12 15:11:23 +0400

    backword compatibility

--- HMBaseExtensions/Extension/UIViewController+StoryBoard.swift
+++ HMBaseExtensions/Extension/UIViewController+StoryBoard.swift
@@ -1,6 +1,18 @@
 import UIKit
 
 public extension UIViewController {
+    /**
+     * @deprecated This method is deprecated starting in version x.x
+     * @note Please use @code instantiateViewControllerForStoryBoardId:type: @endcode instead.
+     */
+    static func instantiateViewControllerForStoryBoardId<T>(_ name: String) -> T {
+        let type = T.self
+        let className = String(describing: type)
+        let bundle = Bundle(for: type as! AnyClass)
+        let storyBoard = UIStoryboard(name: name, bundle: bundle)
+        return storyBoard.instantiateViewController(withIdentifier: className) as! T
+    }
+    
     static func instantiateViewControllerForStoryBoardId<T>(_ name: String, type: T.Type) -> T {
         let className = String(describing: type)
         let bundle = Bundle(for: type as! AnyClass)
